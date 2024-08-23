5ea4f7956455961efd0230ddf062e7502c404ba9
codes-by-chetan <chetanmohite2128@gmail.com>
2024-08-23 15:42:57 +0530

    todo list project push

--- toDoList/index.js
+++ toDoList/index.js
@@ -1,22 +1,9 @@
 const taskInputEl = document.getElementById("task-input");
 const taskInputBtn = document.getElementById("task-input-btn");
 const taskDisplayConatiner = document.getElementById("task-display-conatiner");
-const taskDeleteAllBtn = document.getElementById("task-deleteAll-btn");
-let taskID = 3;
-let tasks = [
-    {
-        id : 1,
-        taskDesc : "do Something"
-    },
-    {
-        id : 2,
-        taskDesc : "do Something"
-    },
-    {
-        id : 3,
-        taskDesc : "do Something"
-    }
-];
+
+let taskID = 0;
+let tasks = [];
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -33,14 +20,16 @@ function displayTasks() {
         tdesc = tasks[i].taskDesc;
         istaskcomplete = tasks[i].isCompleted;
         iscompletetext = istaskcomplete ? "checked":"";
-        todolist = `<div class="task" id="task-${tid}">
+        todolist = `<div class="task ${iscompletetext}" id="task-${tid}">
                         <p>${tdesc}</p>
-                        <div>
-                            <input type="checkbox" class="mark-${istaskcomplete}" name="mark-${tid}" id="mark-${tid}" onclick="marker(${tid})" ${iscompletetext}>
+                        <div class="task-details">
+                            <input type="checkbox" class="checkbox-task  mark-${istaskcomplete}" name="mark-${tid}" id="mark-${tid}" onclick="marker(${i})" ${iscompletetext}>
                             <label for="mark-${tid}">Mark task as complete</label><br>
                         </div>
-                        <button class="delete-this" onclick="removeTask(${tid})">Delete</button>
-                        <button class="edit-this" onclick="editTask(${tid}, this)">Edit</button>
+                        <div>
+                            <button class="delete-this-btn" onclick="removeTask(${i})">Delete</button>
+                            <button class="edit-this-btn" onclick="editTask(${i}, this)">Edit</button>
+                        </div>
                     </div>`;
         taskDisplayConatiner.innerHTML += todolist;
     }
@@ -73,7 +62,7 @@ function localSave() {
 
 function taskSave(inputValue){
     taskID++;
-    tasks.push({
+    tasks.unshift({
         id : taskID,
         taskDesc : inputValue,
         isCompleted : false
@@ -81,82 +70,63 @@ function taskSave(inputValue){
     localSave();
 }
 
-function removeTask(tid) {
-
-    console.log(tasks,tid, typeof tid);
-    if (tid === 'all') {
+function removeTask(i) {
+    
+    if (i === 'all') {
         localStorage.removeItem("tasks");
         localStorage.removeItem("tasks");
         localCollect()
     } else {
-        for(let i = 0; i < tasks.length; i++){
-            console.log("looking for matching id ");
-            if (tasks[i].id === tid){
-                console.log("match found");
-                tasks.splice(i , 1);
-                console.log("delete task of id ", tid);
-                break;
-            }
-        }
+        let tid = tasks[i].id;
+        tasks.splice(i , 1);
+        console.log("deleted task of id ", tid);
+        
         localSave();
     }
 }
 
-function editTask(tid, btn) {
-    let tdesc,istaskcomplete,iscompletetext;
+function editTask(i, btn) {
+    let tid,tdesc,istaskcomplete,iscompletetext;
     const taskEl = btn.parentElement.parentElement;
     taskEl.innerHTML = "";
-    let editEl;
-    for(let i = 0; i < tasks.length; i++){
-        console.log("looking for matching id ");
-        if (tasks[i].id === tid){
-            tdesc = tasks[i].taskDesc;
-            istaskcomplete = tasks[i].isCompleted;
-            iscompletetext = istaskcomplete ? "checked":"";
-            console.log("match found");
-            editEl = `<label class="edit-task-input-label" for="task-${tid}-input">Edit your task and hit Save</label>
-                        <input type="text" class="edit-task-input" id="task-${tid}-input" name="task-${tid}-input" value="${tdesc}" />
-                        <input type="checkbox" class="edit-task-checkbox" id="task-${tid}-checkbox" name="task-${tid}-checkbox" value="" ${iscompletetext}>
-                        <label class="edit-task-checkbox-label" for="task-${tid}-checkbox">Mark task as complete</label>
-                        <button onclick="saveEditedTask(${tid}, this)">Save</button>`;
+    let editEl;  
+    tid = tasks[i].id;
+    tdesc = tasks[i].taskDesc;
+    istaskcomplete = tasks[i].isCompleted;
+    iscompletetext = istaskcomplete ? "checked":"";
+
+    editEl = `<div class="edit-task-input-div">
+                <label class="edit-task-input-label" for="task-${tid}-input">Edit your task and hit Save</label>
+                <input type="text" class="edit-task-input" id="task-${tid}-input" name="task-${tid}-input" value="${tdesc}" />
+                <div>
+                    <input type="checkbox" class="edit-task-checkbox mark-${istaskcomplete}" id="task-${tid}-checkbox" name="task-${tid}-checkbox" value="" ${iscompletetext}>
+                    <label class="edit-task-checkbox-label" for="task-${tid}-checkbox">Mark task as complete</label>
+                </div>
+                </div>
+                <button class="edit-save-btn" onclick="saveEditedTask(${i}, this)">Save</button>`;
             
         
-        } else {
-            let temptid = tasks[i].id;
-            tdesc = tasks[i].taskDesc;
-            istaskcomplete = tasks[i].isCompleted;
-            iscompletetext = istaskcomplete ? "checked":"";
-            editEl = `<div class="task" id="task-${temptid}">
-                            <p>${tdesc}</p>
-                            <div>
-                                <input type="checkbox" class="mark-${istaskcomplete}" name="mark-${temptid}" id="mark-${temptid}" onclick="marker(${temptid})" ${iscompletetext}>
-                                <label for="mark-${temptid}">Mark task as complete</label><br>
-                            </div>
-                            <button class="delete-this" onclick="removeTask(${temptid})">Delete</button>
-                            <button class="edit-this" onclick="editTask(${temptid}, this)">Edit</button>
-                        </div>`;
-            
-        }
-        taskEl.innerHTML += editEl;
-    }
-    
+        
+    taskEl.innerHTML = editEl;   
 }
-function saveEditedTask(tid, btn) {
+
+function saveEditedTask(i, btn) {
+    let tid = tasks[i].id;
     let tdesc,istaskcomplete;
     const newInputEl = document.getElementById(`task-${tid}-input`);
     const newCheckEl = document.getElementById(`task-${tid}-checkbox`);
     tdesc = newInputEl.value;
     istaskcomplete = newCheckEl.checked;
-    console.log("type of iscomplte",typeof istaskcomplete);
-    for(let i = 0; i < tasks.length; i++){
-        console.log("looking for matching id ");
-        if (tasks[i].id === tid){
-            tasks[i].taskDesc = tdesc;
-            tasks[i].isCompleted = istaskcomplete;
-            console.log("task edited id : ",tid);
-            break;
-        }
+
+    tasks[i].taskDesc = tdesc;
+    tasks[i].isCompleted = istaskcomplete;
+    if(istaskcomplete) {
+        tasks.push(tasks.splice(i,1)[0]);
+    } else {
+        tasks.unshift(tasks.splice(i,1)[0]);
     }
+    console.log("task mark updated id : ",tid);
+    
     localSave();
 }
 
@@ -178,19 +148,19 @@ taskInputBtn.addEventListener("click", function() {
     }
 })
 
-taskDeleteAllBtn.addEventListener("click", function() {
-    removeTask('all');
-})
 
-function marker(tid){
+function marker(i){
+    let tid = tasks[i].id;
     let istaskcomplete = document.getElementById(`mark-${tid}`).checked;
-    for(let i = 0; i < tasks.length; i++){
-        console.log("looking for matching id ");
-        if (tasks[i].id === tid){
-            tasks[i].isCompleted = istaskcomplete;
-            console.log("task mark updated id : ",tid);
-            break;
-        }
+
+    tasks[i].isCompleted = istaskcomplete;
+
+    if(istaskcomplete) {
+        tasks.push(tasks.splice(i,1)[0]);
+    } else {
+        tasks.unshift(tasks.splice(i,1)[0]);
     }
+    console.log("task mark updated id : ",tid);
+
     localSave()
 }
